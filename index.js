88dc6043ec4c498b93c7536193cef15e387b5b22
zubairalam1997 <zubairalamraza@gmail.com>
2024-02-11 17:26:36 +0530

    11-2-24 index.js update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {MongoClient} = require("mongodb");
 
 
 const app = express();
-const port = 4000;
+const port = 3006;
 app.use(express.json());
 app.use(cors());
 
@@ -13,33 +13,33 @@ const dbName = "quotes";
 const collectionName = "randomQuotes";
 const randomQuote = require("./db/randomQuote");
 
-app.get('/' , async (req , resp)=>{
-    resp.send("Hello world");
-    let quoteTexts = await randomQuote.find();
-    resp.send({"quote":quoteTexts});
+app.get('/', async (req, resp) => {
+  let quoteTexts = await randomQuote.find();
+  resp.send({ "quote": quoteTexts });
 });
 
 app.get("/home", async (req, resp) => {
-    try {
+  try {
       const client = new MongoClient(url, { useUnifiedTopology: true });
       await client.connect();
-  
+
       const db = client.db(dbName);
       const collection = db.collection(collectionName);
-  
+
       const count = await collection.countDocuments();
       const randomNum = Math.floor(Math.random() * count);
-  
+
       const quoteItem = await collection.findOne({}, { skip: randomNum });
-  
+
       client.close();
-  
+
       resp.send(quoteItem);
-    } catch (err) {
+  } catch (err) {
       console.error(err);
       resp.status(500).send("Error retrieving data");
-    }
-  });
-app.listen(port , ()=>{
-    console.log(`Server is running on ${port}`);
-})
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
